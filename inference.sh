1e2f9425db70aed1549877b85437b9e09a1148bb
cmj5064 <duttu5064@naver.com>
2024-01-19 01:01:16 +0000

    fix: 실험 세팅에 따라 수정

--- inference.sh
+++ inference.sh
@@ -1,8 +1,9 @@
-GROUP=example_group
-NAME=example_name
-CHECKPOINT=logs/${GROUP}/${NAME}/epoch_00051_iteration_000002500_checkpoint.pt
-OUTPUT_MESH=logs/${GROUP}/${NAME}/dtu_24.ply
-CONFIG=logs/${GROUP}/${NAME}/config.yaml
+EXPERIMENT=360_v2
+GROUP=counter
+NAME=test_scale_3
+CHECKPOINT=logs/${GROUP}/${NAME}/epoch_00083_iteration_000010000_checkpoint.pt
+OUTPUT_MESH=logs/${GROUP}/${NAME}/360_v2_counter.ply
+CONFIG=projects/neuralangelo/configs/custom/${EXPERIMENT}.yaml
 RESOLUTION=2048
 BLOCK_RES=128
 GPUS=1  # use >1 for multi-GPU mesh extraction
--- train.sh
+++ train.sh
@@ -1,10 +1,11 @@
-EXPERIMENT=dtu_example
-GROUP=example_group
-NAME=example_name
-# CONFIG=projects/neuralangelo/configs/custom/${EXPERIMENT}.yaml
-CONFIG=projects/neuralangelo/configs/dtu.yaml
-GPUS=1  # use >1 for multi-GPU training!
+EXPERIMENT=360_v2
+GROUP=counter
+NAME=test_scale_3
+CONFIG=projects/neuralangelo/configs/custom/${EXPERIMENT}.yaml
+GPUS=1
 torchrun --nproc_per_node=${GPUS} train.py \
     --logdir=logs/${GROUP}/${NAME} \
     --config=${CONFIG} \
-    --show_pbar
+    --show_pbar \
+    --wandb \
+    --wandb_name=${GROUP}_${NAME}
